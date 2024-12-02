dcd82d23be5860a66c04ccc5f1252291db37f2f1
Lizardin <cool.doo2016@yandex.ru>
2024-12-02 19:51:38 +0300

    github actions test failure check

--- app/main.py
+++ app/main.py
@@ -4,8 +4,8 @@ app = FastAPI()
 
 @app.get("/")
 def read_root():
-    return {"status": "healthy"}
+    return {"status": "healthy?"}
 
 @app.get("/health")
 def health_check():
-    return {"status": "healthy"}
+    return {"status": "healthy?"}
